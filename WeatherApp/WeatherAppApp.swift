6896825706a0a26a5c66d10af5753548fdaa44bb
Arianna Foo <fooar@sheridancollege.ca>
2025-03-05 23:06:07 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Arianna Foo on 2025-03-05.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
