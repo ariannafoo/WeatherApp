bc0560918efd5dfda8926abb47047b37a4b4503b
Arianna Foo <fooar@sheridancollege.ca>
2025-03-06 18:30:10 -0500

    Created LoadingView

---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Arianna Foo on 2025-03-06.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
