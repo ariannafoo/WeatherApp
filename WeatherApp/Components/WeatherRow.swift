2f8b96b10110016d06c46b3233921062600f812d
Arianna Foo <fooar@sheridancollege.ca>
2025-03-06 20:09:25 -0500

    Components - Weather row UI created

---
+++ WeatherApp/Components/WeatherRow.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherRow.swift
+//  WeatherApp
+//
+//  Created by Arianna Foo on 2025-03-06.
+//
+
+import SwiftUI
+
+struct WeatherRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherRow()
+}
